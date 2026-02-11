3f1464fac23ddec9564caf778531995c31b2d292
fred-labs <fred-labs@mailbox.org>
2026-02-11 17:40:19 +0100

    Fix trajectory execution monitoring configuration (#46)

--- panda_moveit_config/launch/move_group.launch.py
+++ panda_moveit_config/launch/move_group.launch.py
@@ -192,7 +192,7 @@ def generate_launch_description():
     trajectory_execution = {
         "allow_trajectory_execution": True,
         "moveit_manage_controllers": False,
-        "execution_duration_monitoring": False,
+        "trajectory_execution.execution_duration_monitoring": False,
         "trajectory_execution.allowed_execution_duration_scaling": 1.5,
         "trajectory_execution.allowed_goal_duration_margin": 0.5,
         "trajectory_execution.allowed_start_tolerance": 0.01,
